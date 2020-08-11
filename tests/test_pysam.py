8da9e6ca74ea90a7ae4f078f0f16afcc8b573095
Vineet Bansal <vineetbansal@protonmail.com>
2020-08-11 15:12:47 -0400

    re-enabled pysam check now that checksum is verified

--- tests/test_pysam.py
+++ tests/test_pysam.py
@@ -9,5 +9,5 @@ DATA_FOLDER = os.path.join(this_dir, 'data')
 
 def test_pysam():
     samfile = os.path.join(DATA_FOLDER, 'SRR5906250-chr22.sorted.bam')
-    assert hashlib.md5(open(samfile, 'rb').read()).hexdigest() == 'a0f00352db430e606c601b850dc80abb'
-    # sam = pysam.AlignmentFile(samfile, 'rb')
+    # assert hashlib.md5(open(samfile, 'rb').read()).hexdigest() == 'a0f00352db430e606c601b850dc80abb'
+    sam = pysam.AlignmentFile(samfile, 'rb')
