a6965c0b7916f77bf6586e4649ffa732d96ec2a7
Vineet Bansal <vineetbansal@protonmail.com>
2020-11-02 17:13:20 -0500

    Using lowercase config keys

--- src/hatchet/utils/BBeval.py
+++ src/hatchet/utils/BBeval.py
@@ -50,7 +50,7 @@ def parsing_arguments(args=None):
     parser.add_argument("-n","--patientnames", required=False, default=config.bbeval.patientnames, type=str, help='Names of patients between apices (default: inferred from filenames)')
     parser.add_argument("-u","--minu", required=False, default=config.bbeval.minu, type=float, help='Minimum proportion of a CNA to be considered subclonal (default: 0.2)"')
     parser.add_argument("-x","--rundir", required=False, default=config.bbeval.rundir, type=str, help='Running directory (default: current directory)')
-    parser.add_argument("-b","--baseCN", required=False, default=config.bbeval.baseCN, type=str, help='Base copy number (default: inferred from tumor ploidy)')
+    parser.add_argument("-b","--baseCN", required=False, default=config.bbeval.basecn, type=str, help='Base copy number (default: inferred from tumor ploidy)')
     parser.add_argument("-sC","--figsizeclones", required=False, default=config.bbeval.figsizeclones, type=str, help='Size of clone plots in the form "(X-SIZE, Y-SIZE)"')
     parser.add_argument("-sP","--figsizecn", required=False, default=config.bbeval.figsizecn, type=str, help='Size of CN plots in the form "(X-SIZE, Y-SIZE)"')
     parser.add_argument("-sG","--figsizegrid", required=False, default=config.bbeval.figsizegrid, type=str, help='Size of grid plots in the form "(X-SIZE, Y-SIZE)"')
