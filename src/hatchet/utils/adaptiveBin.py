76ae60e85796111fdd69bfb2fb7add8403844476
Matt Myers <m@myersmatthew.com>
2021-06-15 13:45:31 -0400

    Remove array forming code from abin

--- src/hatchet/utils/adaptiveBin.py
+++ src/hatchet/utils/adaptiveBin.py
@@ -34,7 +34,6 @@ def main(args=None):
     use_em = args['use_em']
 
     n_workers = min(len(chromosomes), threads)
-    threads_per_worker = int(threads / n_workers)
 
     # Read in centromere locations table
     centromeres = pd.read_table(centromeres, header = None, names = ['CHR', 'START', 'END', 'NAME', 'gieStain'])
@@ -69,7 +68,7 @@ def main(args=None):
     isY = {ch:ch.endswith('Y') for ch in chromosomes}
 
     # form parameters for each worker
-    params = [(stem, all_names, ch, outfile + f'.{ch}', threads_per_worker, 
+    params = [(stem, all_names, ch, outfile + f'.{ch}', 
                chr2centro[ch][0], chr2centro[ch][1], msr, mtr, compressed, arraystems[ch],
                isX[ch] or isY[ch], use_em, phase, blocksize, max_snps_per_block, test_alpha)
               for ch in chromosomes]
@@ -189,92 +188,6 @@ def read_snps(baf_file, ch, all_names, phasefile = None):
     assert np.array_equal(all_names, list(snp_counts.columns)) # make sure that sample order is the same
     
     return np.array(snp_counts.index), np.array(snp_counts), snpsv
-
-def form_counts_array(starts_files, perpos_files, thresholds, chromosome, tabix = 'tabix'):
-    """
-        NOTE: Assumes that starts_files[i] corresponds to the same sample as perpos_files[i]
-        Parameters:
-            starts_files: list of <sample>.<chromosome>.starts.gz files each containing a list of start positions
-            perpos_files: list of <sample>.per-base.bed.gz files containing per-position coverage from mosdepth
-            starts: list of potential bin start positions (thresholds between SNPs)
-            chromosome: chromosome to extract read counts for
-        
-        Returns: <n> x <2d> np.ndarray 
-            entry [i, 2j] contains the number of reads starting in (starts[i], starts[i + 1]) in sample j
-            entry [i, 2j + 1] contains the number of reads covering position starts[i] in sample j
-    """
-    arr = np.zeros((thresholds.shape[0] + 1, len(starts_files) * 2)) # add one for the end of the chromosome
-
-    for i in range(len(starts_files)):
-        # populate starts in even entries
-        fname = starts_files[i]
-        next_idx = 1
-        if fname.endswith('.gz'):
-            f = gzip.open(fname)
-        else:
-            f = open(fname)
-        
-        for line in f:
-            pos = int(line)
-            while next_idx < len(thresholds) and pos > thresholds[next_idx]:
-                next_idx += 1
-                
-            if next_idx == len(thresholds):
-                arr[next_idx - 1, i * 2] += 1
-            elif not (pos == thresholds[next_idx - 1] or pos == thresholds[next_idx]):
-                assert pos > thresholds[next_idx - 1] and pos < thresholds[next_idx], (next_idx, pos)
-                arr[next_idx - 1, i * 2] += 1
-                
-        f.close()
-            
-    for i in range(len(perpos_files)):
-        # populate threshold coverage in odd entries
-        fname = perpos_files[i]
-        #print(datetime.now(), "Reading {}".format(fname))
-        
-        chr_sample_file = os.path.join(fname[:-3] + '.' + chromosome)
-        
-        if not os.path.exists(chr_sample_file):
-            with open(chr_sample_file, 'w') as f:
-                subprocess.run([tabix, fname, chromosome], stdout = f)
-                
-        with open(chr_sample_file, 'r') as records:        
-            idx = 0
-            last_record = None
-            for line in records:
-                tokens = line.split()
-                if len(tokens) == 4:
-                    start = int(tokens[1])
-                    end = int(tokens[2])
-                    nreads = int(tokens[3])
-
-                    while idx < len(thresholds) and thresholds[idx] - 1 < end:
-                        assert thresholds[idx] - 1 >= start
-                        arr[idx, (2 * i) + 1] = nreads
-                        idx += 1
-                    last_record = line
-                        
-            if i == 0:
-                # identify the (effective) chromosome end as the last well-formed record
-                assert idx == len(thresholds)
-                _, _, chr_end, end_reads = last_record.split()
-                chr_end = int(chr_end)
-                end_reads = int(end_reads)
-
-                assert chr_end > thresholds[-1] 
-                assert len(thresholds) == len(arr) - 1
-
-                # add the chromosome end to thresholds
-                thresholds = np.concatenate([thresholds, [chr_end]])
-
-                # count the number of reads covering the chromosome end
-                arr[idx, 0] = end_reads
-                
-        # Clean up after myself (files can be up to 5GB per chromosome and are created quickly from .bed.gz file)
-        if os.path.exists(chr_sample_file):
-            os.remove(chr_sample_file)
-            
-    return arr, thresholds
 
 def adaptive_bins_arm(snp_thresholds, total_counts, snp_positions, snp_counts,
                             min_snp_reads = 2000, min_total_reads = 5000):
@@ -681,7 +594,7 @@ def get_chr_end(stem, all_names, chromosome, compressed):
     
     return last_start 
 
-def run_chromosome(stem, all_names, chromosome, outfile, nthreads, centromere_start, centromere_end, 
+def run_chromosome(stem, all_names, chromosome, outfile, centromere_start, centromere_end, 
          min_snp_reads, min_total_reads, compressed, arraystem, xy, use_em, phasefile, blocksize, max_snps_per_block, test_alpha):
     """
     Perform adaptive binning and infer BAFs to produce a HATCHet BB file for a single chromosome.
@@ -708,34 +621,11 @@ def run_chromosome(stem, all_names, chromosome, outfile, nthreads, centromere_st
             # Load SNP positions and counts for this chromosome
             positions, snp_counts, snpsv = read_snps(os.path.join(stem, 'baf', 'bulk.1bed'), chromosome, all_names, phasefile = phasefile)
             
-        thresholds = np.trunc(np.vstack([positions[:-1], positions[1:]]).mean(axis = 0)).astype(np.uint32)
-        last_idx_p = np.argwhere(thresholds > centromere_start)[0][0]
-        first_idx_q = np.argwhere(thresholds > centromere_end)[0][0]
-        all_thresholds = np.concatenate([[1], thresholds[:last_idx_p], [centromere_start], 
-                                        [centromere_end], thresholds[first_idx_q:]])
+            
+        sp.log(msg=f"Loading intermediate files for chromosome {chromosome}\n", level = "INFO")
+        total_counts = np.loadtxt(arraystem + '.total', dtype = np.uint32)
+        complete_thresholds = np.loadtxt(arraystem + '.thresholds', dtype = np.uint32)
         
-            
-        if arraystem is not None:
-            sp.log(msg=f"Loading intermediate files for chromosome {chromosome}\n", level = "INFO")
-            total_counts = np.loadtxt(arraystem + '.total', dtype = np.uint32)
-            complete_thresholds = np.loadtxt(arraystem + '.thresholds', dtype = np.uint32)
-        else:
-            sp.log(msg=f"Loading chromosome {chromosome}\n", level = "INFO")
-            # Per-position coverage bed files for each sample
-            perpos_files = [os.path.join(stem, 'counts', name + '.per-base.bed.gz') for name in all_names]
-            
-            # Identify the start-positions files for this chromosome
-            starts_files = []
-            for name in all_names:
-                if compressed:
-                    starts_files.append(os.path.join(stem, 'counts', name + '.' + chromosome + '.starts.gz'))    
-                else:
-                    starts_files.append(os.path.join(stem, 'counts', name + '.' + chromosome + '.starts'))    
-
-            #sp.log(msg=f"Loading counts for chromosome {chromosome}\n", level = "INFO")
-            # Load read count arrays from file (also adds end of chromosome as a threshold)
-            total_counts, complete_thresholds = form_counts_array(starts_files, perpos_files, all_thresholds, chromosome) 
-
 
         sp.log(msg=f"Binning p arm of chromosome {chromosome}\n", level = "INFO")
         if len(np.where(positions <= centromere_start)[0]) > 0:
