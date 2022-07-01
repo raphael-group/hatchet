aa5eea1fe121a8148011610f34387442b4c89f12
Vineet Bansal <vineetbansal@protonmail.com>
2022-07-01 13:05:10 -0400

    Protecting check-solver against exceptions so it can be used safely with HATCHet check

--- src/hatchet/utils/check_solver.py
+++ src/hatchet/utils/check_solver.py
@@ -15,41 +15,45 @@ DATA_FOLDER = os.path.join(this_dir, 'data')
 def main(args=None):
     log(msg=f'# The solver you are currently using is {config.compute_cn.solver}\n')
 
-    with tempfile.TemporaryDirectory() as tempdirname:
-        with path(hatchet.data, 'sample.bbc') as bbc_path:
-            input_files_prefix = os.path.splitext(bbc_path)[0]
-            hatchet_main(
-                args=[
-                    '-x',
-                    os.path.join(tempdirname),
-                    '-i',
-                    input_files_prefix,
-                    '-n2',
-                    '-p',
-                    '5',
-                    '-v',
-                    '3',
-                    '-u',
-                    '0.03',
-                    '--mode',
-                    '0',
-                    '-r',
-                    '6700',
-                    '-j',
-                    '1',
-                    '-eD',
-                    '6',
-                    '-eT',
-                    '12',
-                    '-g',
-                    '0.35',
-                    '-l',
-                    '0.6',
-                ]
-            )
+    try:
+        with tempfile.TemporaryDirectory() as tempdirname:
+            with path(hatchet.data, 'sample.bbc') as bbc_path:
+                input_files_prefix = os.path.splitext(bbc_path)[0]
+                hatchet_main(
+                    args=[
+                        '-x',
+                        os.path.join(tempdirname),
+                        '-i',
+                        input_files_prefix,
+                        '-n2',
+                        '-p',
+                        '5',
+                        '-v',
+                        '3',
+                        '-u',
+                        '0.03',
+                        '--mode',
+                        '0',
+                        '-r',
+                        '6700',
+                        '-j',
+                        '1',
+                        '-eD',
+                        '6',
+                        '-eT',
+                        '12',
+                        '-g',
+                        '0.35',
+                        '-l',
+                        '0.6',
+                    ]
+                )
 
-    log(msg=f'# Your current solver {config.compute_cn.solver} seems to be working correctly\n')
-    return True
+    except Exception:
+        return False
+    else:
+        log(msg=f'# Your current solver {config.compute_cn.solver} seems to be working correctly\n')
+        return True
 
 
 if __name__ == '__main__':
