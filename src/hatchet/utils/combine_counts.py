41d7ce07f3f2719ded4760dbb72a7234583612d5
Matt Myers <m@myersmatthew.com>
2024-03-29 09:50:11 -0400

    bugfix: correct-haplotypes length of values vs index issue

--- src/hatchet/utils/combine_counts.py
+++ src/hatchet/utils/combine_counts.py
@@ -906,7 +906,7 @@ def segmented_piecewise(X, pieces=2):
 
 
 def correct_haplotypes(
-    orig_bafs, min_prop_switch=0.01, n_segments=10, min_switch_density=0.1, min_mean_baf=0.45, minmax_al_imb=0.02
+    orig_bafs, min_prop_switch=0.01, n_segments=20, min_switch_density=0.1, min_mean_baf=0.48, minmax_al_imb=0.02
 ):
     n_bins = orig_bafs.shape[0]
     if n_bins == 1:
@@ -948,6 +948,10 @@ def correct_haplotypes(
 
         for idx in np.where(np.diff(ts) == 0)[0]:
             del ts[idx]
+
+        # hacky bugfix: handle bug where backtracking gets to 0 and then adds more segments...
+        new_start = np.where(np.array(ts) == 0)[0][-1]
+        ts = ts[new_start:]
 
         segments = [orig_bafs[ts[i] : ts[i + 1]] for i in range(len(ts) - 1)]
 
@@ -1151,6 +1155,11 @@ def run_chromosome(
                 sp.log(msg='Correcting haplotype switches on p arm...\n', level='STEP')
                 # TODO: pass through other parameters to correct_haplotypes
                 corrected_bafs_p, _ = correct_haplotypes(bafs_p)
+
+                sp.log(msg=
+                       f'{len(bb_p)}, {len(bb_p.SAMPLE.unique())}, {bafs_p.shape}, {corrected_bafs_p.shape} {corrected_bafs_p.flatten().shape}\n',
+                       level='DEBUG')
+
 
                 # flatten these results out and put them back into the BAF array
                 bb_p['ORIGINAL_BAF'] = bb_p.BAF
