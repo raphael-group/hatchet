410311e3f6f45e19871dd6f5a4dc96e23a386c83
RunpengLuo <runpeng.luo@princeton.edu>
2026-03-27 01:27:23 -0400

    Remove edges from 2D scatter dots, keep edges on 1D only

--- src/hatchet/plot/plot_1d2d.py
+++ src/hatchet/plot/plot_1d2d.py
@@ -297,10 +297,7 @@ def plot_2d(
     """
     g0 = sns.JointGrid(x=xvals, y=yvals, hue=hue, palette=palette, xlim=xlim, ylim=ylim)
     g0.refline(x=0.50)
-    g0.plot_joint(
-        sns.scatterplot, s=markersize, legend=False,
-        edgecolor="0.2", linewidth=0.3,
-    )
+    g0.plot_joint(sns.scatterplot, s=markersize, legend=False, edgecolors="none")
     g0.plot_marginals(sns.kdeplot, common_norm=False, linewidth=0.8, fill=False)
     scatter = g0.ax_joint.collections[0]
     scatter.set_rasterized(rasterized)
